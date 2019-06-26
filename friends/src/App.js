730d49f683665302fed33a4c1560b890b3997e20
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-06-25 20:34:56 -0500

    connectec to server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from "axios";
+import Friendlist from './components/Friendlist'
 
-function App() {
+class App extends Component{
+state = {
+  friends: []
+}
+	componentDidMount() {
+		axios.get('http://localhost:5000/friends')
+			.then(response => {this.setState({ friends: response.data})})
+			.catch(err => {
+				console.log('Error:', err)
+      })
+      
+	}
+  render(){
+    console.log(this.state);
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +35,12 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      <p>{this.state.name}</p>
+      <Friendlist friends={this.friends} />
     </div>
   );
-}
+}}
 
 export default App;
---
+++ friends/src/components/Friendlist.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Friendlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState(() => ({ friends: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.friends.map(friends => (
+          console.log(friends)
+        ))}
+      </div>
+    );
+  }
+}
+
+function MovieDetails({ movie }) {
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
+}
