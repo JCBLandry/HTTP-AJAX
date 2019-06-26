7f1ce214311aed3057a149f3b1d6d7181c909b9b
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-06-25 21:40:33 -0500

    friends displaying, base function

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,45 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import axios from "axios";
 import Friendlist from './components/Friendlist'
 
 class App extends Component{
-state = {
-  friends: []
-}
-	componentDidMount() {
-		axios.get('http://localhost:5000/friends')
-			.then(response => {this.setState({ friends: response.data})})
-			.catch(err => {
-				console.log('Error:', err)
-      })
-      
-	}
   render(){
-    console.log(this.state);
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-      <p>{this.state.name}</p>
-      <Friendlist friends={this.friends} />
+      <Friendlist />
     </div>
   );
 }}
 
 export default App;
+
--- friends/src/components/Friendlist.js
+++ friends/src/components/Friendlist.js
@@ -1,54 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-export default class Friendlist extends Component {
+export default class Friendslist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
+      }
     };
   }
 
   componentDidMount() {
     axios
-      .get('http://localhost:5000/friends')
-      .then(response => {
-        this.setState(() => ({ friends: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
+      .get("http://localhost:5000/friends")
+      .then(x => this.setState({ friends: x.data }))
+      .catch(err => console.log(err));
   }
+
 
   render() {
     return (
       <div>
-        {this.state.friends.map(friends => (
-          console.log(friends)
+        {this.state.friends.map(friend => (
+          <div>{friend.name + ", " + friend.age}<br/>{friend.email}<br/></div>
         ))}
+        <div><br />
+        
+        </div>
       </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
 }
